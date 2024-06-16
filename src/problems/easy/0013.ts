d1e47430700fb01f95081680c81cf2a189fed046
merkulov artem <wzx.d.art@gmail.com>
2024-06-16 15:26:41 +0300

    add 0013

---
+++ src/problems/easy/0013.ts
@@ -0,0 +1,23 @@
+const romanToInt = (string: string) => {
+  const map = new Map<string, number>([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const curr = map.get(string[i]) || 0;
+    const next = map.get(string[i + 1]) || 0;
+
+    if (next > curr) count -= curr;
+    else count += curr;
+  }
+
+  return count;
+};
