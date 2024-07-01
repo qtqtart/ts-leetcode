8cb98b3e3603f578e6ea4736b80fbc512db7698a
artem merkulov <qtqt.art@gmail.com>
2024-07-02 00:01:27 +0300

    add 0101

--- src/problems/easy/0070.ts
+++ src/problems/easy/0070.ts
@@ -1,4 +1,4 @@
-const climbStairs = (number: number) =>
+const climbStairs = (number: number): number =>
   number <= 3 ? number : 2 * climbStairs(number - 2) + climbStairs(number - 3);
 
 export default climbStairs;
--- src/problems/easy/0101.ts
+++ src/problems/easy/0101.ts
@@ -1,6 +1,12 @@
 import type { TreeNode } from "@/lib/tree-node";
 
-const isSameTree = (
+const isSymmetric = (root: TreeNode | null) => {
+  if (!root) return true;
+
+  return helper(root.left, root.right);
+};
+
+const helper = (
   treeNode1: TreeNode | null,
   treeNode2: TreeNode | null,
 ): boolean => {
@@ -8,9 +14,9 @@ const isSameTree = (
   if (treeNode1.val !== treeNode2.val) return false;
 
   return (
-    isSameTree(treeNode1.left, treeNode2.left) &&
-    isSameTree(treeNode1.right, treeNode2.right)
+    helper(treeNode1.left, treeNode2.right) &&
+    helper(treeNode1.right, treeNode2.left)
   );
 };
 
-export default isSameTree;
+export default isSymmetric;
